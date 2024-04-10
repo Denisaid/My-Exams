46b3df940b3c3fd320d4b18d41d2b8414c90626b
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2024-04-10 10:25:15 +0300

    JS Applications Exam - 2 April 2022

---
+++ JS Application/JS Applications Exam - 2 April 2022/Pet Care/src/api/pets.js
@@ -0,0 +1,44 @@
+import * as api from './api.js';
+
+const endpoints = {
+    allPets: '/data/pets?sortBy=_createdOn%20desc&distinct=name',
+    petById: '/data/pets/',
+    create: '/data/pets',
+    update: '/data/pets/',
+    delete: '/data/pets/',
+    add: '/data/donation',
+    allDonationCount: (petId) => `/data/donation?where=petId%3D%22${petId}%22&distinct=_ownerId&count`,
+    donationById: (petId, userId) => `/data/donation?where=petId%3D%22${petId}%22%20and%20_ownerId%3D%22${userId}%22&count`
+};
+
+export async function getAllPets() {
+    return api.get(endpoints.allPets);
+}
+
+export async function getPetById(id) {
+    return api.get(endpoints.petById + id);
+}
+
+export async function createPet(data) {
+    return api.post(endpoints.allPets, data);
+}
+
+export async function updatePet(id, data) {
+    return api.put(endpoints.update + id, data);
+}
+
+export async function deletePet(id) {
+    return api.del(endpoints.delete + id);
+}
+
+export async function addDonation(data) {
+    return api.post(endpoints.add, data);
+}
+
+export async function getAllDonationCount(petId) {
+    return api.get(endpoints.allDonationCount(petId));
+}
+
+export async function getDonationById(petId, userId) {
+    return api.get(endpoints.donationById(petId, userId));
+}
---
+++ JS Application/JS Applications Exam - 2 April 2022/Pet Care/src/app.js
@@ -0,0 +1,32 @@
+import page from '../node_modules/page/page.mjs';
+
+import { addRender } from './middlewares/render.js';
+import { addSession } from './middlewares/session.js';
+
+import { homePage } from './views/home.js';
+import { loginPage } from './views/login.js';
+import { registerPage } from './views/register.js';
+import { logout } from './api/users.js';
+import { catalogPage } from './views/catalog.js';
+import { createPage } from './views/create.js';
+import { detailsPage } from './views/details.js';
+import { editPage } from './views/edit.js';
+
+page(addSession);
+page(addRender);
+
+page('/', homePage );
+page('/login', loginPage );
+page('/register', registerPage );
+page('/logout', onLogout );
+page('/catalog', catalogPage );
+page('/create', createPage );
+page('/details/:id', detailsPage );
+page('/edit/:id', editPage );
+
+page.start();
+
+function onLogout(ctx) {
+    logout();
+    ctx.page.redirect('/');
+}
